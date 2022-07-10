504205ac3e12fe6da7cc60bc68a9111b5b5cd7d2
KK09020 <kiku09020@gmail.com>
2022-07-10 10:02:57 +0900

    [Update] 各種Packageのアップデートをしました

--- Skirt/Assets/佐野/Scripts/Chara/Player/Pl_Anim.cs
+++ Skirt/Assets/佐野/Scripts/Chara/Player/Pl_Anim.cs
@@ -36,54 +36,6 @@ public class Pl_Anim : MonoBehaviour {
 
     void FixedUpdate()
     {
-        // ダメージ
-        if (pl_st.isDamaged) {
-            anim.SetBool("isDamaged", true);
-        }
 
-        // 攻撃
-        else if (pl_st.isAttacking) {
-            anim.SetBool("isAttack", true);
-            anim.SetBool("isDamaged", false);
-        }
-
-        // 地上
-        else if (pl_st.isLanding) {
-            anim.SetBool("isLanding", true);        // 地上モーション
-            anim.SetBool("isAttack", false);
-            anim.SetBool("isFloat", false);
-            anim.SetBool("isSwoop", false);
-            
-
-            if (pl.inp_ver != 0) {
-                anim.SetBool("isWalk", true);
-                anim.SetBool("isLanding", false);        // 地上モーション
-            }
-
-            else {
-                anim.SetBool("isWalk", false);
-            }
-        }
-
-        // 3つの状態
-        else {
-            // ふわふわ
-            if (pl_st.isFloating) {
-                anim.SetBool("isFloat", true);
-            }
-
-            // 急降下
-            else if (pl_st.isSwooping) {
-                anim.SetBool("isSwoop", true);
-            }
-
-            // 通常
-            else {
-
-                anim.SetBool("isFloat", false);
-                anim.SetBool("isSwoop", false);
-                anim.SetBool("isLanding", false);
-            }
-        }
     }
 }
